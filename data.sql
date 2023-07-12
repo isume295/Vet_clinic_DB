3ba586045bb10946741a5dcedfd826a6967f05c9
Sumeya <sumeyaibrahim72@gmail.com>
2023-07-12 17:33:43 +0300

    Add data into owners and species table

--- data.sql
+++ data.sql
@@ -17,3 +17,71 @@ VALUES
 ('Boarmon', '2005-06-07', 20.4, true, 7),
 ('Blossom', '1998-10-13', 17, true, 3),
 ('Ditto', '2022-05-14', 22, true, 4);
+
+-- Insert data into ownwers table
+INSERT INTO owners (full_name, age)
+VALUES
+  ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38);
+
+-- insert data into species table
+INSERT INTO species (name)
+VALUES
+  ('Pokemon'),
+  ('Digimon');
+
+-- modify animals to have owners_id information
+ALTER TABLE animals
+ADD COLUMN owner_id INTEGER,
+ADD CONSTRAINT fk_owner
+FOREIGN KEY (owner_id)
+REFERENCES owners(id);
+
+UPDATE animals
+SET owner_id = (
+  SELECT id
+  FROM owners
+  WHERE owners.full_name = 'Sam Smith'
+  LIMIT 1
+)
+WHERE name = 'Agumon';
+
+UPDATE animals
+SET owner_id = (
+  SELECT id
+  FROM owners
+  WHERE owners.full_name = 'Jennifer Orwell'
+  LIMIT 1
+)
+WHERE name IN ('Gabumon', 'Pikachu');
+
+UPDATE animals
+SET owner_id = (
+  SELECT id
+  FROM owners
+  WHERE owners.full_name = 'Bob'
+  LIMIT 1
+)
+WHERE name IN ('Devimon', 'Plantmon');
+
+UPDATE animals
+SET owner_id = (
+  SELECT id
+  FROM owners
+  WHERE owners.full_name = 'Melody Pond'
+  LIMIT 1
+)
+WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+
+UPDATE animals
+SET owner_id = (
+  SELECT id
+  FROM owners
+  WHERE owners.full_name = 'Dean Winchester'
+  LIMIT 1
+)
+WHERE name IN ('Angemon', 'Boarmon');
