e402b9d9b4794f42ace2c58f3e333377aaa0b52a
Sumeya <sumeyaibrahim72@gmail.com>
2023-07-12 09:04:44 +0300

    Insert new column to the table

--- data.sql
+++ data.sql
@@ -5,3 +5,15 @@ VALUES (DEFAULT, 'Agumon', '2020-02-03', 0, true, 10.23),
        (DEFAULT, 'Gabumon', '2018-11-15', 2, true, 8.0),
        (DEFAULT, 'Pikachu', '2021-01-07', 1, false, 15.04),
        (DEFAULT, 'Devimon', '2017-05-12', 5, true, 11.0);
+
+/* Insert new animals into the table */
+
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) 
+VALUES 
+('Charmander', '2020-02-08', -11, false, 0),
+('Plantmon', '2021-11-15', -5.7, true, 2),
+('Squirtle', '1993-04-02', -12.13, false, 3),
+('Angemon', '2005-06-12', -45, true, 1),
+('Boarmon', '2005-06-07', 20.4, true, 7),
+('Blossom', '1998-10-13', 17, true, 3),
+('Ditto', '2022-05-14', 22, true, 4);
